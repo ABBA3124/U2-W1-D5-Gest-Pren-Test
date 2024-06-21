ba347c7b1def7b938aebd6942ff0956114d650a3
ABBA3124 <davideabbadessa73@gmail.com>
2024-06-21 11:00:32 +0200

    Initial commit

---
+++ src/main/java/davide/U2_W1_D5_Gest_Pren_Test/U2W1D5GestPrenTestApplication.java
@@ -0,0 +1,13 @@
+package davide.U2_W1_D5_Gest_Pren_Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2W1D5GestPrenTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2W1D5GestPrenTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/davide/U2_W1_D5_Gest_Pren_Test/U2W1D5GestPrenTestApplicationTests.java
@@ -0,0 +1,13 @@
+package davide.U2_W1_D5_Gest_Pren_Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2W1D5GestPrenTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
